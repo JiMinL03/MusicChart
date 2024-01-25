78b09c18c6a451f45cfeb47cb2ce3255dace27a1
JiMinL <youmeyourmy@naver.com>
2024-01-25 19:22:38 +0900

    스프링부트세팅

---
+++ src/main/java/com/example/musicchart/MainController.java
@@ -0,0 +1,15 @@
+package com.example.musicchart;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	@GetMapping("/MusicCharts")
+	public String root() {
+		return "MusicCharts";
+	}
+}
+//이 코드는 웹에서 http://localhost:8080/MusicCharts로 요청이 들어오면
+//MusicCharts.html파일로 넘어가게 하는 코드야!
+//@GetMapping은 웹에서 요청이 들어오면 서버에서 데이터를 제공하는거!
---
+++ src/main/java/com/example/musicchart/MusicChartsApplication.java
@@ -0,0 +1,14 @@
+package com.example.musicchart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicChartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicChartsApplication.class, args);
+	}
+
+}
+//여긴 신경 안 써도 됨~
